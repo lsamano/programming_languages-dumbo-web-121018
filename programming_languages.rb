e61be6c849ad87a6c63df74e39957661a0193643
Leizl Samano <leizl.samano@gmail.com>
2018-12-04 03:39:22 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,13 @@
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |style|
+    style.each do |program, type_hash|
+      if new_hash[program] == nil #Checks if the city hasn't been added.
+      new_hash[program] = [] #Adds the city key with an empty array.
+    end
+    new_hash[program] << style[program] #Shovels school into the array.
+    end
+  end
+
+  return new_hash
 end
