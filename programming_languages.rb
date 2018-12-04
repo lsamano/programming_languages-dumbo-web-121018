b6b1853db63c369eb3bbeaee5a7e2dd5e28245c5
Leizl Samano <leizl.samano@gmail.com>
2018-12-04 05:50:56 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -5,13 +5,11 @@ def reformat_languages(languages)
     new_hash.merge!(hash_of_programs)
   end
 
-  #Step 2: Add "style: []" to all programs
-  new_hash.each do |program, info|
-    info[:style] = []
-  end
-
-  #Step 3: Shovel :oo or :functional into :style's array
   new_hash.each do |program, type_hash|
+    #Step 2: Add "style: []" to each program
+    type_hash[:style] = []
+
+    #Step 3: Shovel :oo or :functional into :style's array
     languages.each do |style, hash_of_programs|
       if hash_of_programs.has_key?(program)
         new_hash[program][:style] << style
@@ -19,5 +17,5 @@ def reformat_languages(languages)
     end
   end
 
-  return new_hash
+  new_hash
 end
