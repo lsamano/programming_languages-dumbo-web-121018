8b8be279392265eb76ef437371820a3e70940032
Leizl Samano <leizl.samano@gmail.com>
2018-12-04 04:47:43 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,10 @@
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |style|
+    style.each do |program, type_hash|
+      new_hash[program] = type_hash
+    end
+  end
+
+  return new_hash
 end
